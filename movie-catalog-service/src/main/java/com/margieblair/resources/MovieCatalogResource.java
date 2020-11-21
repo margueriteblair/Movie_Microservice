b9801837f8c826f51faa2394bdcf64d24ec5802b
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-20 23:57:42 -0500

    singleton list wow what fake api

--- movie-catalog-service/src/main/java/com/margieblair/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/margieblair/resources/MovieCatalogResource.java
@@ -4,12 +4,17 @@ import com.margieblair.model.CatalogItem;
 
 import java.util.Collections;
 import java.util.List;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 //just that one controller needed to be a rest controller!
 @RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
 
-    public List<CatalogItem> getCatalog(String userId) {
+    @RequestMapping("/{userId}")
+    public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
         return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
     }
 }
