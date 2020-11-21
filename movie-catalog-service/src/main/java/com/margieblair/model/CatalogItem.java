9d0e0ac3596517a4abb92024412a9e39c79cff8e
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-20 23:56:29 -0500

    dummy api where you get transformers no matter what lmao

--- movie-catalog-service/src/main/java/com/margieblair/model/CatalogItem.java
+++ movie-catalog-service/src/main/java/com/margieblair/model/CatalogItem.java
@@ -6,6 +6,12 @@ public class CatalogItem {
     private String desc;
     private int rating;
 
+    public CatalogItem(String name, String desc, int rating) {
+        this.name = name;
+        this.rating = rating;
+        this.desc = desc;
+    }
+
     public int getRating() {
         return rating;
     }
--- movie-catalog-service/src/main/java/com/margieblair/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/margieblair/resources/MovieCatalogResource.java
@@ -1,12 +1,15 @@
 package com.margieblair.resources;
 
 import com.margieblair.model.CatalogItem;
+
+import java.util.Collections;
+import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 //just that one controller needed to be a rest controller!
 @RestController
 public class MovieCatalogResource {
 
     public List<CatalogItem> getCatalog(String userId) {
-
+        return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
     }
 }
