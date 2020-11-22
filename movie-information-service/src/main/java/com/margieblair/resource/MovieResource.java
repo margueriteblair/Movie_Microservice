ffced1878bec053835dbf1e0d82dbd61415676ac
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-21 19:04:07 -0500

    filled out movie resource

--- movie-information-service/src/main/java/com/margieblair/resource/MovieResource.java
+++ movie-information-service/src/main/java/com/margieblair/resource/MovieResource.java
@@ -1,8 +1,14 @@
 package main.java.com.margieblair.resource;
 
+
+import main.java.com.margieblair.models.Movie;
+
+@RequestController
+@RequestMapping("/movies")
 public class MovieResource {
 
-    public Movie getMovieInfo() {
-
+    @RequestMapping("/{movieId}")
+    public Movie getMovieInfo(@PathVariable("modieId") String movieId) {
+        return new Movie(movieId, "Test Name");
     }
 }
