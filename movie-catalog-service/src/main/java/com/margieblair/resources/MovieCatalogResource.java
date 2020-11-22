96e13a879d8eaf45ca927c96106125eefdb07888
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-21 22:03:15 -0500

    copied the movie class from our other service

--- movie-catalog-service/src/main/java/com/margieblair/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/com/margieblair/resources/MovieCatalogResource.java
@@ -7,10 +7,13 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.margieblair.model.Movie;
 import com.margieblair.model.Rating;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 //just that one controller needed to be a rest controller!
 @RestController
 @RequestMapping("/catalog")
@@ -21,9 +24,16 @@ public class MovieCatalogResource {
         //get all rated movie ids
         //for each movie id call movie infor service and get details
         //put them all together
-        List<Rating> ratings = Arrays.asList(new Rating("1234", 4), new Rating("4567", 3));
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getForObject("http://localhost:8081/movies/foo", Movie,)
 
-        return ratings.stream().map(rating -> new CatalogItem("Transformers", "test", 4)).collect(Collectors.toList());
-//        return Collections.singletonList(new CatalogItem("Transformers", "Test", 4));
+        List<Rating> ratings = Arrays.asList(
+                new Rating("1234", 4),
+                new Rating("4567", 3));
+
+        return ratings.stream().map(rating -> {
+
+            new CatalogItem("Transformers", "test", 4)
+        }).collect(Collectors.toList());
     }
 }
