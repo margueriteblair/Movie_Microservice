cffe96ca7aa55b8de02e1e1d1302c85a28a094e8
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-21 19:52:14 -0500

    resource made for the ratings service that can actually fetch the data on its specific port

--- rating-data-service/src/main/java/com/margieblair/ratings/RatingsResource.java
+++ rating-data-service/src/main/java/com/margieblair/ratings/RatingsResource.java
@@ -1,4 +1,12 @@
 package main.java.com.margieblair.ratings;
 
+@RestController
+@RequestMapping("/ratingsdata")
 public class RatingsResource {
+
+    @RequestMapping("/{movieId}")
+    public Rating getRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId,4);
+    }
+
 }
