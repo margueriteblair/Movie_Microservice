20a27c20fb3ca848cb3358c0324f73f697aa0291
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-22 23:48:07 -0500

    running tests

--- movie-information-service/src/main/java/com/margieblair/MovieInformationServiceApplication.java
+++ movie-information-service/src/main/java/com/margieblair/MovieInformationServiceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MovieInformationServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello fuckers");
 		SpringApplication.run(MovieInformationServiceApplication.class, args);
 	}
 
