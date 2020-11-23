eb3adad6fb608f740a42eb2d35c3e1bdd202575e
Marguerite Blair <marguerite.blair@gmail.com>
2020-11-23 00:30:10 -0500

    i am literally shocked

--- movie-information-service/src/main/java/com/margieblair/MovieInformationServiceApplication.java
+++ movie-information-service/src/main/java/com/margieblair/MovieInformationServiceApplication.java
@@ -1,13 +1,22 @@
 package com.margieblair;
 
+import org.springframework.aop.framework.autoproxy.BeanNameAutoProxyCreator;
+import org.springframework.beans.factory.BeanCreationException;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieInformationServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MovieInformationServiceApplication.class, args);
+		try {
+			SpringApplication.run(MovieInformationServiceApplication.class, args);
+		} catch (BeanCreationException ex) {
+			System.out.println("NO BEANS HERE FOOL");
+		}
 	}
 
 }
